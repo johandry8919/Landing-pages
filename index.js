d4f3a4e34fc4cd1fa13b8d141c3ea787edff049b
johandry8919 <rojas.rojas440@gmail.com>
2022-02-14 19:35:02 -0400

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-import 'animate.css';
+const button = document.querySelector('button');
+const popup = document.querySelector('.popup-wrapper');
+const close = document.querySelector('.popup-close');
+ 
+button.addEventListener('click', () => {
+    popup.style.display = 'block';
+});
+ 
+close.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+ 
+popup.addEventListener('click', e => {
+    // console.log(e);
+    if(e.target.className === 'popup-wrapper') {
+        popup.style.display = 'none';
+    }
+});
